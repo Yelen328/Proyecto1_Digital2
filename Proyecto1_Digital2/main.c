93b44d3efa754e480cfcb67de03623a2eb8eade6
Yelen328 <cot231076@uvg.edu.gt>
2026-02-10 00:08:39 -0600

    Initial commit

---
+++ Proyecto1_Digital2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Proyecto1_Digital2.c
+ *
+ * Created: 10/2/2026 00:07:48
+ * Author : yelen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
